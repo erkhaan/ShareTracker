7e67e8dcdf59e92f067633df3534c25a21a489e7
Erkhaan Struchkov <notykt@gmail.com>
2022-09-23 17:29:10 +0600

    Fix brackets in ViewModel

--- ShareTracker/ViewModels/ViewModel.swift
+++ ShareTracker/ViewModels/ViewModel.swift
@@ -2,30 +2,30 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-class companyViewModel: ObservableObject{
+class companyViewModel: ObservableObject {
 	@Published var companies: [company]
 
-	init(){
+	init() {
 		self.companies = csvImport()
-		for i in 0..<companies.count{
+		for i in 0..<companies.count {
 			fetchCompanyStocks(i)
 		}
 	}
 
 	// MARK: API
 
-	func fetchCompanyStocks(_ i: Int){
+	func fetchCompanyStocks(_ i: Int) {
 		let url = "https://finnhub.io/api/v1/quote?symbol="+self.companies[i].ticker+"&token=c1rvmd2ad3ifb04kehfg"
 
-		AF.request(url).responseJSON{ response in
-			switch response.result{
+		AF.request(url).responseJSON { response in
+			switch response.result {
 			case .success(let value):
 				let json = JSON(value)
-				guard let c = json["c"].double else{
+				guard let c = json["c"].double else {
 					print(json["c"].error!)
 					return
 				}
-				guard let pc = json["pc"].double else{
+				guard let pc = json["pc"].double else {
 					print(json["pc"].error!)
 					return
 				}
@@ -39,10 +39,10 @@ class companyViewModel: ObservableObject{
 
 	// MARK: Methods
 
-	func favoritePressed(_ i: Int){
-		if(companies[i].isFavorite){
+	func favoritePressed(_ i: Int) {
+		if companies[i].isFavorite {
 			companies[i].buttonColor = RGB(r: 0.5, g: 0.5, b: 0.5)
-		}else{
+		} else {
 			companies[i].buttonColor = RGB(r: 246/256, g: 197/256, b: 67/256)
 		}
 		companies[i].isFavorite.toggle()
@@ -50,11 +50,11 @@ class companyViewModel: ObservableObject{
 
 	// Searching
 
-	func textFound(_ a: String, _ b: String) -> Bool{
+	func textFound(_ a: String, _ b: String) -> Bool {
 		a.lowercased().contains(b.lowercased())
 	}
 
-	func searchResult(_ company: company, _ searchText: String) -> Bool{
+	func searchResult(_ company: company, _ searchText: String) -> Bool {
 		let s: String = company.name + company.ticker
 		return textFound(s,searchText) || searchText.isEmpty
 	}
@@ -62,37 +62,37 @@ class companyViewModel: ObservableObject{
 
 // MARK: Formatting
 
-extension companyViewModel{
-	func formatCurrentPrice(_ c: Double) -> String{
+extension companyViewModel {
+	func formatCurrentPrice(_ c: Double) -> String {
 		String(format: "$%.2f",c)
 	}
 
-	func formatDifference(_ value: Double) -> String{
+	func formatDifference(_ value: Double) -> String {
 		var s: String
-		if value >= 0.0{
+		if value >= 0.0 {
 			s = "+$" + String(format:"%.2f",value)
-		}else{
+		} else {
 			s = "-$" + String(format:"%.2f",-value)
 		}
 		return s
 	}
 
-	func formatDifferenceInPercent(value: Double, pc: Double) -> String{
+	func formatDifferenceInPercent(value: Double, pc: Double) -> String {
 		String(format:"(%.2f%%)",value/pc*100)
 	}
 
-	func formatStockDifferenceInString(value: Double, pc: Double) -> String{
+	func formatStockDifferenceInString(value: Double, pc: Double) -> String {
 		formatDifference(value) + " " + formatDifferenceInPercent(value: value, pc: pc)
 	}
 
-	func getStockColor(_ value: Double) -> RGB{
-		if value >= 0{
+	func getStockColor(_ value: Double) -> RGB {
+		if value >= 0 {
 			return RGB(r: 45/255, g: 173/255, b: 94/255)
 		}
 		return RGB(r: 1, g: 0, b: 0)
 	}
 
-	func getStockView(c: Double, pc: Double) -> stockInfo{
+	func getStockView(c: Double, pc: Double) -> stockInfo {
 		var stockView = stockInfo()
 		let value = c - pc
 
@@ -106,26 +106,26 @@ extension companyViewModel{
 
 // MARK: Persistence
 
-extension companyViewModel{
-	private static var documentsFolder: URL{
+extension companyViewModel {
+	private static var documentsFolder: URL {
 		do {
 			return try FileManager.default.url(for: .documentDirectory,
 											   in: .userDomainMask,
 											   appropriateFor: nil,
 											   create: false)
-		} catch  {
+		} catch {
 			fatalError("Can't find document directory")
 		}
 	}
 
-	private static var fileURL: URL{
+	private static var fileURL: URL {
 		return documentsFolder.appendingPathComponent("companies.data")
 	}
 
-	func load(){
+	func load() {
 		DispatchQueue.global(qos: .background).async { [weak self] in
 			guard let data = try? Data(contentsOf: Self.fileURL) else { return }
-			guard let companiesData = try? JSONDecoder().decode([company].self, from: data) else{
+			guard let companiesData = try? JSONDecoder().decode([company].self, from: data) else {
 				print("Can't decode saved companies data")
 				return
 			}
@@ -135,14 +135,18 @@ extension companyViewModel{
 		}
 	}
 
-	func save(){
+	func save() {
 		DispatchQueue.global(qos: .background).async { [weak self] in
-			guard let companies = self?.companies else { fatalError("Self out of scope")}
-			guard let data = try? JSONEncoder().encode(companies) else { fatalError("Error encoding data")}
-			do{
+			guard let companies = self?.companies else {
+                fatalError("Self out of scope")
+            }
+			guard let data = try? JSONEncoder().encode(companies) else {
+                fatalError("Error encoding data")
+            }
+			do {
 				let outfile = Self.fileURL
 				try data.write(to: outfile)
-			} catch{
+			} catch {
 				fatalError("Can't write file")
 			}
 		}
